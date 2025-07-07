dcc1484da00fc39fd1822da4d12ab519203f54a7
Ainirina <rakotosonirina6@gmail.com>
2025-07-07 23:53:38 +0300

    model 2

---
+++ backend_spring_bookiku/src/main/java/com/bookiku/backend/Model/Admin.java
@@ -0,0 +1,53 @@
+package com.bookiku.backend.Model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ADMINS")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_admin")
+    private int idAdmin;
+
+    @Column(name = "nom_utilisateur", length = 50, nullable = false)
+    private String nomUtilisateur;
+
+    @Column(name = "mot_de_passe", nullable = false)
+    private String motDePasse;
+
+    // Constructeurs
+    public Admin() {}
+
+    public Admin(int idAdmin, String nomUtilisateur, String motDePasse) {
+        this.idAdmin = idAdmin;
+        this.nomUtilisateur = nomUtilisateur;
+        this.motDePasse = motDePasse;
+    }
+
+    // Getters et Setters
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNomUtilisateur() {
+        return nomUtilisateur;
+    }
+
+    public void setNomUtilisateur(String nomUtilisateur) {
+        this.nomUtilisateur = nomUtilisateur;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+}
