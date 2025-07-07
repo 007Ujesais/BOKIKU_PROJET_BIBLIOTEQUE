c495cc8d986bdf6f14f290c5e66dbd4d0c21dfb1
Nantenaina isaac <ujesaisnantah@gmail.com>
2025-07-07 23:56:49 +0300

    v

--- backend_spring_bookiku/src/main/java/com/bookiku/backend/Controllers/AdminController.java
+++ backend_spring_bookiku/src/main/java/com/bookiku/backend/Controllers/AdminController.java
@@ -1,5 +1,15 @@
-package com.bookiku.backend.Controllers;
+package com.bookiku.backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
-    
+
+    @GetMapping("/")
+    public String welcomeAdmin(Model model) {
+        model.addAttribute("nom", "Admin");
+        return "welcome-admin";
+    }
 }
