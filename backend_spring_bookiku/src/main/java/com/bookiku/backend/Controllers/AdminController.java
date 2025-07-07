2653d8d13e536074ada4a551a40353200a7c8240
Ainirina <rakotosonirina6@gmail.com>
2025-07-07 23:55:32 +0300

    correction model

--- backend_spring_bookiku/src/main/java/com/bookiku/backend/Controllers/AdminController.java
+++ backend_spring_bookiku/src/main/java/com/bookiku/backend/Controllers/AdminController.java
@@ -1,4 +1,4 @@
-package com.bookiku.backend.Controllers;
+package com.bookiku.backend.controllers;
 
 public class AdminController {
     
--- backend_spring_bookiku/src/main/java/com/bookiku/backend/Model/Adherents.java
+++ backend_spring_bookiku/src/main/java/com/bookiku/backend/Model/Adherents.java
@@ -1,4 +1,4 @@
-package com.bookiku.backend.Model;
+package com.bookiku.backend.model;
 // Enum pour Profil (ADHERENTS)
 import jakarta.persistence.*;
 
