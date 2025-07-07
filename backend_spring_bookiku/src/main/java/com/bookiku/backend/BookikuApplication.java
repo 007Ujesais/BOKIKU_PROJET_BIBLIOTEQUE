d14fe37bbb380b15ca7800a379b7805cfe493d7e
Nantenaina isaac <ujesaisnantah@gmail.com>
2025-07-07 22:35:30 +0300

    SRING

---
+++ backend_spring_bookiku/src/main/java/com/bookiku/backend/BookikuApplication.java
@@ -0,0 +1,13 @@
+package com.bookiku.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookikuApplication.class, args);
+	}
+
+}
---
+++ backend_spring_bookiku/src/test/java/com/bookiku/backend/BookikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookiku.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
